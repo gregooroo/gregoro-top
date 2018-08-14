387e8a07360157f71ba95cd15984e4cad817376f
Mateusz <gregooroo@gmail.com>
2018-08-14 22:42:51 +0200

    Styling home - details

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,21 @@
+const coinDetails = {
+  oxycoin: {
+    poolWebsite: "https://pool.oxycoin.gregoro.top",
+    delegate: "/delegate/oxycoin",
+    mainWebsite: "https://oxycoin.io",
+    chat: "https://oxycoin.slack.com"
+  },
+  shift: {
+    poolWebsite: "https://pool.shiftnrg.gregoro.top",
+    delegate: "/delegate/shift",
+    mainWebsite: "https://www.shiftnrg.org",
+    chat: "https://shiftnrg.ryver.com/application/login"
+  }
+};
+
 module.exports = (req, res) => {
   res.render("home", {
-    title: "Home page"
+    title: "Home page",
+    coinDetails
   });
 };
