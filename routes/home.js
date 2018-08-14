83290fee00b1c38270166a4b6a3a6b8d463b058f
Mateusz <gregooroo@gmail.com>
2018-08-14 23:23:52 +0200

    Move coinDetails into config.json file

--- routes/home.js
+++ routes/home.js
@@ -1,17 +1,4 @@
-const coinDetails = {
-  oxycoin: {
-    poolWebsite: "https://pool.oxycoin.gregoro.top",
-    delegate: "/delegate/oxycoin",
-    mainWebsite: "https://oxycoin.io",
-    chat: "https://oxycoin.slack.com"
-  },
-  shift: {
-    poolWebsite: "https://pool.shiftnrg.gregoro.top",
-    delegate: "/delegate/shift",
-    mainWebsite: "https://www.shiftnrg.org",
-    chat: "https://shiftnrg.ryver.com/application/login"
-  }
-};
+const { coinDetails } = require("./../config.json");
 
 module.exports = (req, res) => {
   res.render("home", {
