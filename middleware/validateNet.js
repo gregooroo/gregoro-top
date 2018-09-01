15f2ad0441780100c3a34f62f18c78097da7f110
Mateusz <gregooroo@gmail.com>
2018-09-01 23:05:33 +0200

    Add valid net to request object

--- middleware/validateNet.js
+++ middleware/validateNet.js
@@ -3,5 +3,6 @@ const availableNets = ["testnet", "mainnet"];
 module.exports = (req, res, next) => {
   const { net } = req.params;
   if (!availableNets.includes(net)) return res.redirect("/empty");
+  req.net = net;
   next();
 };
