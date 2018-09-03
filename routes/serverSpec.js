fbb0ec27fb903c04becd46ab03c6945befe29055
Mateusz <gregooroo@gmail.com>
2018-09-03 21:40:35 +0200

    Finish serverSpec route

--- routes/serverSpec.js
+++ routes/serverSpec.js
@@ -1,3 +1,11 @@
+const { capitalizeFirstLetter } = require("../helpers/helpers");
+const { serverDetails } = require("../config.json");
+
 module.exports = (req, res) => {
-  res.send("It works");
+  res.render("serverSpec", {
+    title: `${capitalizeFirstLetter(req.coin)} mainnet servers details`,
+    coin: req.coin,
+    net: req.net,
+    serverDetails: serverDetails[req.coin][req.net]
+  });
 };
